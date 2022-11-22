057de5bc028b8ff97c0f34961b640d641a8a9b24
michellefragozo <>
2022-11-22 15:39:53 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+#this is my first phyton program  
+""""
+ this is code is  written un phyton  3.8 
+interpreted  by the consol 
+"""
+print("Hello World")
+print (50)
+print (3.10)
+print (50 "letters", 3.10)
+print ("hello world ",end = " ")
+print  ("world")
---
+++ replit.nix
@@ -0,0 +1,6 @@
+{ pkgs }: {
+    deps = [
+        pkgs. hello world                                                                                                                 
+        pkgs.deno
+    ];
+}
